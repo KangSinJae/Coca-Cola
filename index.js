a29500e63a34ec186391077784c4e6348f65601a
KangSinJae <tlswo5303@naver.com>
2023-07-30 04:18:29 +0900

    Brand페이지 완성

--- index.js
+++ index.js
@@ -33,6 +33,7 @@
 
 let didScroll = false;
 let paralaxTitles = document.querySelectorAll('.home-img');
+let movetext = document.querySelectorAll('.move-text')
 
 const scrollInProgress = () => {
   didScroll = true
@@ -42,6 +43,7 @@ const raf = () => {
   if(didScroll) {
     paralaxTitles.forEach((element, index) => {
       element.style.transform = "translateX("+ window.scrollY / -35 + "%)"
+		
     })
     didScroll = false;
   }
@@ -49,5 +51,52 @@ const raf = () => {
 }
 
 
+const move = () => {
+  if(didScroll) {
+    movetext.forEach((element, index) => {
+      element.style.transform = "translateX("+ window.scrollY /  + "%)"
+		
+    })
+    didScroll = false;
+  }
+  requestAnimationFrame(move);
+}
+
 requestAnimationFrame(raf);
+requestAnimationFrame(move);
 window.addEventListener('scroll', scrollInProgress)
+
+
+
+
+
+// 스크롤 이벤트를 사용하여 클래스 토글 기능 구현
+document.addEventListener('DOMContentLoaded', function() {
+  const scrollArea = document.querySelector('.brand-container');
+	const originalImage = document.getElementById('original-image');
+  const changedImage = document.getElementById('changed-image');
+
+  function handleScroll() {
+    // 스크롤 영역의 위치와 높이 계산
+    const scrollAreaTop = scrollArea.offsetTop;
+    const scrollAreaHeight = scrollArea.offsetHeight;
+
+    // 현재 스크롤 위치 계산
+    const scrollPosition = window.scrollY;
+
+    // 스크롤 영역에 도달했는지 여부를 확인하여 클래스 추가/제거
+    if (scrollPosition >= scrollAreaTop && scrollPosition < scrollAreaTop + scrollAreaHeight) {
+      scrollArea.classList.add('active');
+			originalImage.style.display = 'none';
+      changedImage.style.display = 'block';
+    } else {
+      scrollArea.classList.remove('active');
+			originalImage.style.display = 'block';
+      changedImage.style.display = 'none';
+    }
+  }
+
+  // 스크롤 이벤트를 등록하여 스크롤 시 handleScroll 함수 호출
+  window.addEventListener('scroll', handleScroll);
+});
+
